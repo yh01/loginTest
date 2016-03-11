04bcfd07ed522699d6044fbc7bfa72fbe4ae194a
yuki hoshino <hoshih0sh1@outlook.jp>
2016-03-11 18:02:42 +0900

    更新

--- src/dto/EditDTO.java
+++ src/dto/EditDTO.java
@@ -2,6 +2,8 @@ package dto;
 
 public class EditDTO {
 	private int userId;
+	private String userName;
+	private String password;
 
 	public int getUserId() {
 		return userId;
@@ -9,6 +11,22 @@ public class EditDTO {
 
 	public void setUserId(int userId) {
 		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
